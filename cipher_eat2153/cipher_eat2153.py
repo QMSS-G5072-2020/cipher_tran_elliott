b2e7ca8142b16aad40ee93671f9f555a9c87bf1a
Elliott <eat2153@columbia.edu>
2020-11-14 22:03:32 -0500

    Update cipher_eat2153.py

---
+++ cipher_eat2153/cipher_eat2153.py
@@ -0,0 +1,30 @@
+def cipher(text, shift, encrypt=True):
+    """
+    Each letter is replaced by a letter some fixed number of positions down the alphabet.
+
+    Parameters
+    ---
+    text:
+    shift:
+    encrypt:
+
+    Returns
+    ---
+
+
+    Examples
+    ---
+    import cipher_eat2153
+    
+    """
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+    for c in text:
+        index = alphabet.find(c)
+        if index == -1:
+            new_text += c
+        else:
+            new_index = index + shift if encrypt == True else index - shift
+            new_index %= len(alphabet)
+            new_text += alphabet[new_index:new_index+1]
+    return new_text
